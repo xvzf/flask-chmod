dba7ff6d99ba118469f0e6414f301cf73ae4d679
Matthias Riegler <riegler.matthias2@gmail.com>
2018-06-28 20:55:49 +0200

    Add redis to travis, dump python 2.7 support

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(here, "README.rst"), encoding="utf-8") as f:
 
 setup(
     name="flask-chown",
-    version="0.1.2",
+    version="0.2.0",
     description="Flask permissions the UNIX way using owner and groups",
     long_description=long_description,
     url="https://github.com/xvzf/flask-chown",
@@ -30,7 +30,6 @@ setup(
         "License :: OSI Approved :: Apache Software License",
         "Framework :: Flask",
         "Programming Language :: Python",
-        "Programming Language :: Python :: 2",
         "Programming Language :: Python :: 3",
     ],
 
