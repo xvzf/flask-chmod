03291fdfc0e8e136e3ba977968960cc522cd96ae
Matthias Riegler <riegler.matthias2@gmail.com>
2018-07-11 17:40:27 +0200

    Fixes issue when using current_user from flask-login

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(here, "README.rst"), encoding="utf-8") as f:
 
 setup(
     name="flask-chown",
-    version="0.2.0",
+    version="0.2.1",
     description="Flask permissions the UNIX way using owner and groups",
     long_description=long_description,
     url="https://github.com/xvzf/flask-chown",
